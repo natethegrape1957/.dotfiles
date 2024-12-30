d76dfff9f384df273d2ac3d4f4448f649e42336f
natethegrape1957 <nathangreat770@gmail.com>
2024-12-31 00:16:46 +0900

    feat: added hyprland config

--- initsymlink.sh
+++ initsymlink.sh
@@ -2,3 +2,7 @@ ln -s ~/.dotfiles/.bashrc ~/
 ln -s ~/.dotfiles/.gitconfig ~/
 ln -s ~/.dotfiles/starship.toml ~/.config/
 ln -s ~/.dotfiles/nvim/ ~/.config/
+ln -s ~/.dotfiles/hypr/ ~/.config/
+ln -s ~/.dotfiles/kitty/ ~/.config/
+ln -s ~/.dotfiles/mako/ ~/.config/
+ln -s ~/.dotfiles/waybar/ ~/.config/
